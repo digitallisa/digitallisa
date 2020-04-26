d5cc319c55c360d31012ad8b7bba6164f7827713
Nils Neuhaus <nils168@gmail.com>
2020-04-26 09:35:38 +0200

    Example services
    Co-authored-by: diddyxking <info@danielux.de>

---
+++ components/services/serviceCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-actions>
+      <v-btn to="/chatbot" nuxt></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ pages/services.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="display-1">{{ title }}</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="3">
+        <service-card title="Create google maps page" />
+      </v-col>
+      <v-col cols="3">
+        <service-card title="Add Facebook Page" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import ServiceCard from '~/components/services/serviceCard.vue'
+export default {
+  components: {
+    ServiceCard
+  },
+  data() {
+    return {
+      title: 'Services'
+    }
+  }
+}
+</script>
+
+<style></style>
