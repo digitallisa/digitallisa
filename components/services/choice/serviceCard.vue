119069028e9fb24997d7d72092ca121f99965293
Nils Neuhaus <nils168@gmail.com>
2020-04-26 18:48:02 +0200

    Done services

---
+++ components/services/choice/serviceCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card width="100%">
+    <v-container>
+      <v-row>
+        <v-col cols="auto">
+          <v-list-item-avatar
+            class="ml-4"
+            tile
+            size="80"
+            color="grey"
+          ></v-list-item-avatar>
+        </v-col>
+        <v-col cols="5">
+          <v-row>
+            <v-card-title>{{ service.title }}</v-card-title>
+          </v-row>
+          <v-row>
+            <v-card-subtitle>
+              {{ service.description }}
+            </v-card-subtitle>
+          </v-row>
+        </v-col>
+        <v-col cols="3">
+          <v-row>
+            <v-col>
+              <v-btn text>
+                Start Tutorial Now
+                <v-icon right>mdi-arrow-right</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col>
+              <v-btn dark color="black" @click="$emit('serviceChosen')">
+                Add Premium Service
+                <v-icon right>mdi-plus</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    service: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/services/choice.vue
+++ pages/services/choice.vue
@@ -2,73 +2,95 @@
   <v-container>
     <v-row>
       <v-col>
-        <h1>Select the Services you need:</h1>
-        <v-card>
-          <v-list shaped>
-            <v-list-item-group v-model="model" multiple>
-              <template v-for="(item, i) in items">
-                <v-divider v-if="!item" :key="`divider-${i}`"></v-divider>
-
-                <v-list-item
-                  v-else
-                  :key="`item-${i}`"
-                  :value="item"
-                  active-class="blue--text text--accent-4"
-                >
-                  <template v-slot:default="{ active, toggle }">
-                    <v-list-item-content>
-                      <v-list-item-title v-text="item"></v-list-item-title>
-                    </v-list-item-content>
-
-                    <v-list-item-action>
-                      <!-- <v-switch value input-value="true" @click="toggle"></v-switch> -->
-                      <v-checkbox
-                        :input-value="active"
-                        :true-value="item"
-                        color="deep-blue accent-4"
-                        @click="toggle"
-                      ></v-checkbox>
-                    </v-list-item-action>
-                  </template>
-                </v-list-item>
-              </template>
-            </v-list-item-group>
-          </v-list>
-        </v-card>
-        <v-col>
-          <v-card>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <v-list-item-title class="headline">Price</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-actions>
-              <v-btn text>Buy</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
+        <h1>Choose your services</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="8">
+        <v-container>
+          <v-row>
+            <v-col v-for="(service, index) in services" :key="index" cols="12">
+              <service-card :service="service" @serviceChosen="serviceChosen()"
+            /></v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+      <v-divider vertical></v-divider>
+      <v-col cols="3">
+        <v-row justify="center">
+          <v-col cols="12">
+            <div class="display-1">Chosen services</div>
+          </v-col>
+          <v-col
+            v-for="(chosenService, index) in chosenServices"
+            :key="index"
+            cols="12"
+          >
+            <v-row justify="space-between" align="center">
+              <v-col cols="auto">
+                <span class="font-weight-thin">{{ chosenService.title }}</span>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn icon>
+                  <v-icon>mdi-close</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="12">
+            <v-divider></v-divider>
+          </v-col>
+          <v-col cols="12">
+            <span class="font-weight-bold">Price </span
+            ><span>{{ overallPrice }}€</span>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn dark color="black">
+              Buy Now
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import ServiceCard from '~/components/services/choice/serviceCard.vue'
 export default {
+  components: {
+    ServiceCard
+  },
   data: () => ({
-    items: [
-      'Website',
-      'Google Business',
-      'Google Search optimization',
-      'Facebook',
-      'Instagram',
-      'LinkedIn',
-      'Twitter',
-      '',
-      'Continous Maintenance Service',
-      'Continous Online Ads Service'
-    ],
-    model: ['Carrots']
-  })
+    chosenServices: [],
+    services: [
+      {
+        title: 'Google Business',
+        subtitle: 'Set-up Google Business Account'
+      },
+      {
+        title: 'Facebook',
+        subtitle: 'Set-up Facebook'
+      }
+    ]
+  }),
+  computed: {
+    overallPrice() {
+      let overallPrice = 0
+      this.chosenServices.forEach((element) => {
+        overallPrice = overallPrice + element.price
+      })
+      return overallPrice
+    }
+  },
+  methods: {
+    serviceChosen() {
+      this.chosenServices.push({
+        title: 'Google Business',
+        price: 50
+      })
+    }
+  }
 }
 </script>
 
