a852c2f4ef1cb0791e3000203f219ad9a1d307ae
Daniel Lux <daniel@MacBook-Pro.local>
2020-04-26 12:17:26 +0200

    Homepage
    Co-authored-by: nils168@live.de <nils168@live.de>

--- components/index/mainContainer.vue
+++ components/index/mainContainer.vue
@@ -1,33 +1,29 @@
 <template>
   <v-container fluid class="pa-0 ma-0">
-    <v-row class="grey" align="center" style="height:400px" no-gutters>
-      <v-col cols="12" md="6">
-        <v-sheet>
-          IMAGE HERE
-        </v-sheet>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-row dense>
-          <v-col cols="12">
-            <h4 class="display-2 font-weight-black text-center">
-              Bring your business online with Lisa.
-            </h4>
-          </v-col>
-          <v-col cols="12">
-            <h4 class="display-2 font-weight-black text-center"></h4>
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-row justify="center" align="center" style="height:500px">
-      <v-col cols="auto">
-        <v-btn to="/chatbot" nuxt x-large outlined>Talk to Lisa now.</v-btn>
+    <v-row class="light-blue" align="center" style="height:400px" no-gutters>
+      <v-col cols="6">
+        <v-container class="ml-6">
+          <v-row justify="center" dense>
+            <v-col cols="12">
+              <h4 class="white--text display-3 font-weight-black">
+                Bring your business online with Lisa.
+              </h4>
+            </v-col>
+            <v-col cols="12">
+              <v-row>
+                <v-col cols="auto">
+                  <v-btn to="/chatbot" nuxt x-large filled>
+                    Chat With Lisa
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
     </v-row>
     <v-row justify="center" align="center">
-      <v-col class="title" cols="auto">
-        You have the control.
-      </v-col>
+      <v-col class="title" cols="auto">You have the control.</v-col>
     </v-row>
     <v-row justify="center" align="center">
       <v-col cols="auto">
